9b12b30d5abe11eeddcb634800b6f5283809d853
gabefrancisco <gabegit2@gmail.com>
2026-02-16 12:18:16 -0300

    implemented socket creation

--- main.c
+++ main.c
@@ -24,6 +24,30 @@
 
 int main()
 {
-    int server_fd;  // File descriptor of server socket
-    int new_socket; // File descriptor of the client socket
+    int server_fd;              // File descriptor of server socket
+    int new_socket;             // File descriptor of the client socket
+    int val_read;               // Number of bytes read
+    struct sockaddr_in address; // Struct for address(IP/Port) definitions
+    int addrlen = sizeof(address);
+
+    // Model of a fixed HTTP response(Header + Body)
+    // HTTP/1.1 200 OK: Succes state
+    // Content-Type: (plain text)
+    // Content-Length: Size of the response
+    // \r\n\r\n: Mark the end of the Header and the start of Body
+
+    char *test = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Lenght: 12\r\n\r\nHello World.God bless you!";
+
+    // -----------------------------------
+    // 1 - Socket creation
+    // -----------------------------------
+    // AF_INET      = IPv4
+    // SOCK_STREAM  = TCP
+    // 0            = Protocol(0 lets the system chose de standard to TCP)
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("Error while creating the socket");
+        exit(EXIT_FAILURE);
+    }
 }
